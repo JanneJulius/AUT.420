7e7749bab07fb5c84e30900602f5a98e42bda9ed
JanneJulius <janne.lahdentausta25@gmail.com>
2024-11-03 16:24:00 +0200

    Apply .gitignore rules and remove unwanted files

---
+++ PulpProcessApp/PulpProcessApp/Helpers/ObservableCollectionTarget.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using NLog.Targets;
+
+[Target("ObservableCollectionTarget")]
+public class ObservableCollectionTarget : TargetWithLayout
+{
+    private readonly LogViewModel _logViewModel;
+
+    public ObservableCollectionTarget(LogViewModel logViewModel)
+    {
+        _logViewModel = logViewModel;
+    }
+
+    protected override void Write(LogEventInfo logEvent)
+    {
+        string logMessage = Layout.Render(logEvent);
+        _logViewModel.AddLog(logMessage);
+    }
+}
